11a5b3315ec6bd3416a5ce798dc8db37771d9c5e
James Cori <jamescori@gmail.com>
2020-08-07 10:27:20 -0400

    Forgot the config vars

--- config/default.js
+++ config/default.js
@@ -29,6 +29,22 @@ module.exports = {
   AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET || '',
   AUTH0_PROXY_SERVER_URL: process.env.AUTH0_PROXY_SERVER_URL,
 
+  INFORMIX: {
+    server: process.env.INFORMIX_SERVER || 'informixoltp_tcp', // informix server
+    database: process.env.INFORMIX_DATABASE || 'tcs_catalog', // informix database
+    host: process.env.INFORMIX_HOST || 'localhost', // host
+    protocol: process.env.INFORMIX_PROTOCOL || 'onsoctcp',
+    port: process.env.INFORMIX_PORT || '2021', // port
+    db_locale: process.env.INFORMIX_DB_LOCALE || 'en_US.57372',
+    user: process.env.INFORMIX_USER || 'informix', // user
+    password: process.env.INFORMIX_PASSWORD || '1nf0rm1x', // password
+    maxsize: parseInt(process.env.MAXSIZE) || 0,
+    minpool: parseInt(process.env.MINPOOL, 10) || 1,
+    maxpool: parseInt(process.env.MAXPOOL, 10) || 60,
+    idleTimeout: parseInt(process.env.IDLETIMEOUT, 10) || 3600,
+    timeout: parseInt(process.env.TIMEOUT, 10) || 30000
+  },
+
   // Topcoder APIs
   V5_CHALLENGE_API_URL: process.env.V5_CHALLENGE_API_URL || 'http://localhost:4000/v5/challenges',
   V5_CHALLENGE_TYPE_API_URL: process.env.V5_CHALLENGE_TYPE_API_URL || 'http://localhost:4000/v5/challenge-types',
