d4be527eb21bab3b67682aee69042e8171ba4f71
James Cori <jamescori@gmail.com>
2020-08-10 14:56:20 -0400

    Fixing build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -eo pipefail
-APP_NAME=$1
 UPDATE_CACHE=""
-docker-compose -f docker/docker-compose.yml build $APP_NAME
-docker create --name app $APP_NAME:latest
+docker-compose -f docker/docker-compose.yml build legacy-challenge-processor
+docker create --name app legacy-challenge-processor:latest
 
 if [ -d node_modules ]
 then
   mv package-lock.json old-package-lock.json
-  docker cp app:/$APP_NAME/package-lock.json package-lock.json
+  docker cp app:/app/package-lock.json package-lock.json
   set +eo pipefail
   UPDATE_CACHE=$(cmp package-lock.json old-package-lock.json)
   set -eo pipefail
@@ -18,5 +17,6 @@ fi
 
 if [ "$UPDATE_CACHE" == 1 ]
 then
-  docker cp app:/$APP_NAME/node_modules .
+  docker cp app:/app/node_modules .
 fi
+
