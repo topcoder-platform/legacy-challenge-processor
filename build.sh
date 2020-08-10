b52f06249ddf9b89c399ceb251dc814f038a63b2
James Cori <jamescori@gmail.com>
2020-08-10 10:29:32 -0400

    Trying updated build with no package-lock

--- build.sh
+++ build.sh
@@ -2,14 +2,13 @@
 set -eo pipefail
 APP_NAME=$1
 UPDATE_CACHE=""
-#docker-compose -f docker/docker-compose.yml build $APP_NAME
-docker build -f docker/Dockerfile -t $APP_NAME:latest .
+docker-compose -f docker/docker-compose.yml build $APP_NAME
 docker create --name app $APP_NAME:latest
 
 if [ -d node_modules ]
 then
   mv package-lock.json old-package-lock.json
-  docker cp app:/app/package-lock.json package-lock.json
+  docker cp app:/$APP_NAME/package-lock.json package-lock.json
   set +eo pipefail
   UPDATE_CACHE=$(cmp package-lock.json old-package-lock.json)
   set -eo pipefail
@@ -19,5 +18,5 @@ fi
 
 if [ "$UPDATE_CACHE" == 1 ]
 then
-  docker cp app:/app/node_modules .
+  docker cp app:/$APP_NAME/node_modules .
 fi
