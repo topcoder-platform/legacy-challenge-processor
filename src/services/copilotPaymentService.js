89a3f781215084ba3f9cb856519b198118d5dfdc
James Cori <jamescori@gmail.com>
2020-10-13 20:48:31 -0400

    rewriting the copilot payment service to manipulate the resource_info table as well.

--- src/services/copilotPaymentService.js
+++ src/services/copilotPaymentService.js
@@ -3,7 +3,12 @@ const _ = require('lodash')
 const util = require('util')
 const helper = require('../common/helper')
 
-const QUERY_GET_COPILOT_PAYMENT = 'SELECT limit 1 * FROM project_info WHERE project_info_type_id = 49 AND project_id = %d'
+const COPILOT_PAYMENT_PROJECT_INFO_ID = 49
+const COPILOT_PAYMENT_RESOURCE_INFO_ID = 7
+const COPILOT_RESOURCE_ROLE_ID = 14
+
+const QUERY_GET_COPILOT_RESOURCE_FOR_CHALLENGE = `SELECT limit 1 resource_id as resourceid FROM resource WHERE project_id = %d AND resource_role_id = ${COPILOT_RESOURCE_ROLE_ID}`
+const QUERY_GET_COPILOT_PAYMENT = `SELECT limit 1 * FROM project_info WHERE project_info_type_id = ${COPILOT_PAYMENT_PROJECT_INFO_ID} AND project_id = %d`
 const QUERY_INSERT_COPILOT_PAYMENT = `
   INSERT INTO project_info
     (
@@ -16,9 +21,14 @@ const QUERY_INSERT_COPILOT_PAYMENT = `
       modify_date
     )
   VALUES
-    (?, 49, ?, ?, CURRENT, ?, CURRENT)`
-const QUERY_UPDATE_COPILOT_PAYMENT = 'UPDATE project_info SET value = ?, modify_user = ?, modify_date = CURRENT WHERE project_info_type_id = 49 AND project_id = ?'
-const QUERY_DELETE_COPILOT_PAYMENT = 'DELETE FROM project_info WHERE project_info_type_id = 49 AND project_id = ?'
+    (?, ${COPILOT_PAYMENT_PROJECT_INFO_ID}, ?, ?, CURRENT, ?, CURRENT)`
+const QUERY_UPDATE_COPILOT_PAYMENT = `UPDATE project_info SET value = ?, modify_user = ?, modify_date = CURRENT WHERE project_info_type_id = ${COPILOT_PAYMENT_PROJECT_INFO_ID} AND project_id = ?`
+const QUERY_DELETE_COPILOT_PAYMENT = `DELETE FROM project_info WHERE project_info_type_id = ${COPILOT_PAYMENT_PROJECT_INFO_ID} AND project_id = ?`
+
+// const QUERY_GET_COPILOT_RESOURCE_PAYMENT = `select * from resource_info where resource_id = %d AND resource_info_type_id = ${COPILOT_PAYMENT_RESOURCE_INFO_ID}`
+const QUERY_INSERT_COPILOT_RESOURCE_PAYMENT = `INSERT INTO resource_info (resource_id, resource_info_type_id, value, create_user, create_date, modify_user, modify_date) VALUES (?, ${COPILOT_PAYMENT_RESOURCE_INFO_ID}, ?, ?, CURRENT, ?, CURRENT)`
+const QUERY_UPDATE_COPILOT_RESOURCE_PAYMENT = `UPDATE resource_info SET value = ?, modify_user = ?, modify_date = CURRENT WHERE resource_id = ? AND resource_info_type_id = ${COPILOT_PAYMENT_RESOURCE_INFO_ID}`
+// const QUERY_DELETE_COPILOT_RESOURCE_PAYMENT = `DELETE FROM resource_info WHERE resource_id = ? AND resource_info_type_id = ${COPILOT_PAYMENT_RESOURCE_INFO_ID}`
 
 /**
  * Prepare Informix statement
@@ -42,20 +52,22 @@ async function prepare (connection, sql) {
 async function setCopilotPayment (challengeLegacyId, amount, createdBy, updatedBy) {
   const connection = await helper.getInformixConnection()
   try {
-    // await connection.beginTransactionAsync()
+    await connection.beginTransactionAsync()
+    const copilotResourceId = await getCopilotResourceId(connection, challengeLegacyId)
     const copilotPayment = await getCopilotPayment(connection, challengeLegacyId)
-    if (copilotPayment) {
-      if (!amount) {
-        await deleteCopilotPayment(connection, challengeLegacyId)
-      } else if (_.toString(copilotPayment.value) !== _.toString(amount)) {
-        await updateCopilotPayment(connection, challengeLegacyId, amount, updatedBy)
+    if (copilotResourceId) {
+      if (copilotPayment && amount != null && amount >= 0) {
+        logger.debug(`Copilot payment exists, updating: ${challengeLegacyId}`)
+        return updateCopilotPayment(connection, copilotResourceId, challengeLegacyId, amount, updatedBy)
       }
-    } else {
-      await createCopilotPayment(connection, challengeLegacyId, amount, createdBy)
+      logger.debug(`NO Copilot payment exists, creating: ${challengeLegacyId}`)
+      return createCopilotPayment(connection, copilotResourceId, challengeLegacyId, amount, createdBy)
     }
+    logger.debug(`No copilot assigned, removing any payments for legacy ID: ${challengeLegacyId}`)
+    return deleteCopilotPayment(connection, copilotResourceId, challengeLegacyId)
   } catch (e) {
     logger.error(`Error in 'setCopilotPayment' ${e}`)
-    // await connection.rollbackTransactionAsync()
+    await connection.rollbackTransactionAsync()
     throw e
   } finally {
     await connection.closeAsync()
@@ -73,16 +85,30 @@ async function getCopilotPayment (connection, challengeLegacyId) {
 }
 
 /**
+ * Gets the copilot resource id to use in creating resource payment
+ * @param {Object} connection
+ * @param {Number} challengeLegacyId
+ */
+async function getCopilotResourceId (connection, challengeLegacyId) {
+  const result = await connection.queryAsync(util.format(QUERY_GET_COPILOT_RESOURCE_FOR_CHALLENGE, challengeLegacyId))
+  return _.get(result, '[0]', null)
+}
+
+/**
  * Create the copilot payment record
  * @param {Object} connection the connection
  * @param {Number} challengeLegacyId the legacy challenge id
  * @param {Number} amount the $ amount of the copilot payment
  * @param {String} createdBy the create user handle
  */
-async function createCopilotPayment (connection, challengeLegacyId, amount, createdBy) {
+async function createCopilotPayment (connection, copilotResourceId, challengeLegacyId, amount, createdBy) {
   const query = await prepare(connection, QUERY_INSERT_COPILOT_PAYMENT)
-  logger.debug(`Create Copilot Payment Query ${query} Values: ${[challengeLegacyId, amount, createdBy, createdBy]}`)
-  return query.executeAsync([challengeLegacyId, amount, createdBy, createdBy])
+  logger.debug(`Create Copilot Payment Values: ${[challengeLegacyId, amount, createdBy, createdBy]}`)
+  await query.executeAsync([challengeLegacyId, amount, createdBy, createdBy])
+
+  const resourceQuery = await prepare(connection, QUERY_INSERT_COPILOT_RESOURCE_PAYMENT)
+  logger.debug(`Create Copilot Resource Payment Values: ${[copilotResourceId, amount, createdBy, createdBy]}`)
+  return resourceQuery.executeAsync([copilotResourceId, amount, createdBy, createdBy])
 }
 
 /**
@@ -91,10 +117,14 @@ async function createCopilotPayment (connection, challengeLegacyId, amount, crea
  * @param {Number} challengeLegacyId
  * @param {*} updatedBy the update user handle
  */
-async function updateCopilotPayment (connection, challengeLegacyId, newValue, updatedBy) {
+async function updateCopilotPayment (connection, copilotResourceId, challengeLegacyId, newValue, updatedBy) {
   const query = await prepare(connection, QUERY_UPDATE_COPILOT_PAYMENT)
-  logger.debug(`Update Copilot Payment Query ${query} Values: ${[newValue, updatedBy, challengeLegacyId]}`)
-  return query.executeAsync([newValue, updatedBy, challengeLegacyId])
+  logger.debug(`Update Copilot Payment Query Values: ${[newValue, updatedBy, challengeLegacyId]}`)
+  await query.executeAsync([newValue, updatedBy, challengeLegacyId])
+
+  const resourceQuery = await prepare(connection, QUERY_UPDATE_COPILOT_RESOURCE_PAYMENT)
+  logger.debug(`Update Copilot Resource Payment Values: ${[newValue, updatedBy, copilotResourceId]}`)
+  return resourceQuery.executeAsync([newValue, updatedBy, copilotResourceId])
 }
 
 /**
@@ -102,10 +132,15 @@ async function updateCopilotPayment (connection, challengeLegacyId, newValue, up
  * @param {Object} connection
  * @param {Number} challengeLegacyId
  */
-async function deleteCopilotPayment (connection, challengeLegacyId) {
+async function deleteCopilotPayment (connection, copilotResourceId, challengeLegacyId) {
   const query = await prepare(connection, QUERY_DELETE_COPILOT_PAYMENT)
-  logger.debug(`Delete Copilot Payment Query ${query} Values: ${[challengeLegacyId]}`)
-  return query.executeAsync([challengeLegacyId])
+  logger.debug(`Delete Copilot Payment Values: ${[challengeLegacyId]}`)
+  await query.executeAsync([challengeLegacyId])
+
+  // the function checks if there's a copilot. If there's no copilot, you can't delete the resource for the challenge because there's no resource id
+  // const resourceQuery = await prepare(connection, QUERY_DELETE_COPILOT_RESOURCE_PAYMENT)
+  // logger.debug(`Delete Copilot Resource Values: ${[copilotResourceId]}`)
+  // return resourceQuery.executeAsync([copilotResourceId])
 }
 
 module.exports = {
