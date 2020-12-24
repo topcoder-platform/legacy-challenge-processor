ad03fb4418fbad771b251b95737ac1f74d0700c4
James Cori <jcori@topcoder.com>
2020-12-24 16:50:44 -0500

    I can't spel

--- src/services/ProcessorService.js
+++ src/services/ProcessorService.js
@@ -403,7 +403,7 @@ async function processCreate (message) {
   logger.debug('processCreate :: beforeTry')
   try {
     logger.info(`processCreate :: Skip Forums - ${config.V4_CHALLENGE_API_URL}?filter=skipForum=true body: ${JSON.stringify({ param: _.omit(saveDraftContestDTO, ['groupsToBeAdded', 'groupsToBeDeleted']) })}`)
-    const newChallenge = await helper.postRequest(`${config.V4_CHALLENGE_API_URL}?filterskipForum=true`, { param: _.omit(saveDraftContestDTO, ['groupsToBeAdded', 'groupsToBeDeleted']) }, m2mToken)
+    const newChallenge = await helper.postRequest(`${config.V4_CHALLENGE_API_URL}?filter=skipForum=true`, { param: _.omit(saveDraftContestDTO, ['groupsToBeAdded', 'groupsToBeDeleted']) }, m2mToken)
 
     let forumId = 0
     if (message.payload.legacy && message.payload.legacy.forumId) {
