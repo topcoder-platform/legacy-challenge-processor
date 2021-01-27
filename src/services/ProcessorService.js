468d96a6012bdc7d402dbb12c8c67b6bcc763ab4
Thomas Kranitsas <kranitsasthomas@gmail.com>
2021-01-27 18:55:04 +0200

    fix issue with phase status

--- src/services/ProcessorService.js
+++ src/services/ProcessorService.js
@@ -36,7 +36,7 @@ async function syncChallengePhases (legacyId, v5Phases) {
         (!v5Equivalent.isOpen && _.toInteger(phase.phase_status_id) === constants.PhaseStatusTypes.Open)) {
         const newStatus = v5Equivalent.isOpen
           ? constants.PhaseStatusTypes.Open
-          : (new Date().getTime() >= new Date(v5Equivalent.scheduledStartDate).getTime() ? constants.PhaseStatusTypes.Scheduled : constants.PhaseStatusTypes.Closed)
+          : (new Date().getTime() <= new Date(v5Equivalent.scheduledStartDate).getTime() ? constants.PhaseStatusTypes.Scheduled : constants.PhaseStatusTypes.Closed)
         // update phase
         await timelineService.updatePhase(
           phase.project_phase_id,
