5ad10235f38ec83359880c2ff07daf5468715753
Thomas Kranitsas <kranitsasthomas@gmail.com>
2021-02-02 16:58:52 +0200

    Add a bandaid fix for the phase reset issue

--- src/services/ProcessorService.js
+++ src/services/ProcessorService.js
@@ -661,6 +661,10 @@ async function processUpdate (message) {
         logger.info('Activating challenge...')
         await activateChallenge(legacyId)
         logger.info('Activated!')
+        // HACK: reset the phases after activation
+        logger.info('Hack! Reset the phases after activation...')
+        await syncChallengePhases(message.payload.legacyId, message.payload.phases)
+        logger.info('Done!')
       }
       if (message.payload.status === constants.challengeStatuses.Completed && challenge.currentStatus !== constants.challengeStatuses.Completed) {
         if (message.payload.task.isTask) {
