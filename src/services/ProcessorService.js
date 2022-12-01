2a0e5ee06fefc5c956d4084b9613cff07ddba263
eisbilir <emre.isbilir@gmail.com>
2022-12-01 19:28:22 +0300

    fix phase status flip

--- src/services/ProcessorService.js
+++ src/services/ProcessorService.js
@@ -129,7 +129,7 @@ async function syncChallengePhases (legacyId, v5Phases, createdBy, isSelfService
           logger.debug(`Will update phase ${phaseName}/${v5Equivalent.name} from ${phase.duration} to duration ${v5Equivalent.duration * 1000} milli`)
           const newStatus = v5Equivalent.isOpen
             ? constants.PhaseStatusTypes.Open
-            : (new Date().getTime() <= new Date(v5Equivalent.scheduledEndDate).getTime() ? constants.PhaseStatusTypes.Scheduled : constants.PhaseStatusTypes.Closed)
+            : _.toInteger(phase.phase_status_id)
           await timelineService.updatePhase(
             phase.project_phase_id,
             legacyId,
